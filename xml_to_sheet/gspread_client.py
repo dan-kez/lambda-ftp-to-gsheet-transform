c0c9396ad1cc0cd32553d4f195ad1e54d72a645e
Daniel Kezerashvili <daniel.kez@gmail.com>
2020-02-03 20:30:15 -0500

    Use spreadsheet name for lookup

--- xml_to_sheet/gspread_client.py
+++ xml_to_sheet/gspread_client.py
@@ -1,7 +1,7 @@
 import logging
 from functools import lru_cache
 
-from gspread import SpreadsheetNotFound
+from gspread import SpreadsheetNotFound, WorksheetNotFound
 from oauth2client.service_account import ServiceAccountCredentials
 import gspread
 import settings
@@ -23,13 +23,20 @@ def get_gspread_client():
 @lru_cache(maxsize=1)
 def get_sheet(attempt_to_create=True):
     try:
-        worksheet = get_gspread_client().open(settings.GOOGLE_SHEET_NAME)
+        spreadsheet = get_gspread_client().open(settings.GOOGLE_SHEET_NAME)
     except SpreadsheetNotFound as e:
         if attempt_to_create:
-            worksheet = get_gspread_client().create(settings.GOOGLE_SHEET_NAME)
+            spreadsheet = get_gspread_client().create(settings.GOOGLE_SHEET_NAME)
         else:
             raise e
 
-    logging.info('Google Sheet URL: https://docs.google.com/spreadsheets/d/{}'.format(worksheet.id))
-    sheet = worksheet.sheet1
+    logging.info('Google Sheet URL: https://docs.google.com/spreadsheets/d/{}'.format(spreadsheet.id))
+    try:
+        sheet = spreadsheet.worksheet(settings.GOOGLE_WORKSHEET_NAME)
+    except WorksheetNotFound as e:
+        if attempt_to_create:
+            spreadsheet.add_worksheet(settings.GOOGLE_WORKSHEET_NAME)
+        else:
+            raise e
+
     return sheet
